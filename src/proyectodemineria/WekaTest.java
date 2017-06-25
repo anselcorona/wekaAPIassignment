eaf7130bb5a4297c0be4e0b5aff52475d591a179
anselcorona <anselcorona@gmail.com>
2017-06-25 14:51:43 -0400

    ok

--- src/proyectodemineria/WekaTest.java
+++ src/proyectodemineria/WekaTest.java
@@ -48,26 +48,6 @@ public class WekaTest {
         Evaluation eval = new Evaluation(train);
         eval.evaluateModel(cls, test);
         System.out.println(eval.toSummaryString("\nResults\n======\n", false));
-        
-        /*Remove rm = new Remove();
-        rm.setAttributeIndices("1");  // remove 1st attribute
-        // classifier
-        J48 j48 = new J48();
-        j48.setUnpruned(true);        // using an unpruned J48
-        // meta-classifier
-        FilteredClassifier fc = new FilteredClassifier();
-        fc.setFilter(rm);
-        fc.setClassifier(j48);
-        // train and make predictions
-        fc.buildClassifier(train);
-        for (int i = 0; i < test.numInstances(); i++) {
-            double pred = fc.classifyInstance(test.instance(i));
-            System.out.print("ID: " + test.instance(i).value(0));
-            System.out.print(", real: " + test.classAttribute().value((int) test.instance(i).classValue()));
-            System.out.println(", predecido: " + test.classAttribute().value((int) pred));
-          }
-        */
-        
     }
     
     public String classifyJ48(String datafile, int porcentaje) throws IOException, Exception{
